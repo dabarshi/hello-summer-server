2dab31a751cf5dcd05dd1da1431d285831771c33
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-06-15 23:49:20 +0600

    add a classes collection and a classes get api

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("summerDB").collection("users");
+    const classCollection = client.db("summerDB").collection("classes");
+    // classes api
+    app.get('/classes', async(req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    })
+    
+    // user api 
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
